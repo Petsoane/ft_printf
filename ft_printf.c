81400e7442fb48f49c39a359db32369f8c876e02
Lebogang PETSOANE <lpetsoan@c5r7s2.wethinkcode.co.za>
2019-07-04 08:10:14 +0200

    Half way through

--- ft_printf.c
+++ ft_printf.c
@@ -6,11 +6,14 @@
 /*   By: lpetsoan <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/06/18 06:56:54 by lpetsoan          #+#    #+#             */
-/*   Updated: 2019/06/18 09:58:38 by lpetsoan         ###   ########.fr       */
+/*   Updated: 2019/07/04 08:06:49 by lpetsoan         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_printf.h"
+
+int		print_spec(const char **f, va_list list);
+int		print_id(const char f, va_list list);
 
 int		ft_printf(char const *f, ...)
 {
@@ -22,20 +25,54 @@ int		ft_printf(char const *f, ...)
 		if (*f == '%')
 		{
 			f++;
-			if (*f == 'i' || *f == 'd' || *f == 'D')
-				ft_putnbr(va_arg(list , int));
-			else if (*f == 'f' || *f == 'F')
-				ft_putfloat(va_arg(list, double));
-			else if (*f == 's' || *f == 'S')
-				ft_putstr(va_arg(list, char *));
-			else if (*f == 'c' || *f == 'C')
-				ft_putchar(va_arg(list, int));
-			else if (*f == 'o' || *f == 'O')
-				ft_putoctal(va_arg(list, int));
+			if  (print_spec(&f, list) == -1)
+				return (-1);
 		}
 		else
+		{
 			ft_putchar(*f);
-		f++;
+			f++;
+		}
 	}
 	return (0);
 }
+
+
+int		print_spec(char const **f, va_list list)
+{
+	if (print_id(**f, list) == -1)
+		return (-1);
+	(*f)++;
+	return (0);
+}
+
+int print_id(const char f, va_list list)
+{
+	if (f == 'i' || f == 'd' || f == 'D')
+	{
+		ft_putnbr(va_arg(list , int));
+		return (0);
+	}
+	else if (f == 'f' || f == 'F')
+	{
+		ft_putfloat(va_arg(list, double), 10000);
+		return (0);
+	}
+	else if (f == 's' || f == 'S')
+	{
+		ft_putstr(va_arg(list, char *));
+		return (0);
+	}
+	else if (f == 'c' || f == 'C')
+	{
+		ft_putchar(va_arg(list, int));
+		return (0);
+	}
+	else if (f == 'o' || f == 'O')
+	{
+		ft_baseconv(va_arg(list, int), 8);
+		return (0);	
+	}
+	return (-1);
+}
+
